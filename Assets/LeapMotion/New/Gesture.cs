b259731dc5ed756231c232559ec1052b71ba3a95
JKirkYuan <kirkyuan@cmail.carleton.ca>
2017-05-05 11:36:15 -0600

    Setup simple UI for Leap Motion in Unity

---
+++ Assets/LeapMotion/New/Gesture.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Leap;
+
+public class Gesture : MonoBehaviour {
+
+    public Button button;
+    Controller controller = new Controller();
+
+	// Use this for initialization
+	void Start () {
+        Button btn = button.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        Debug.Log("You clicked the button");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
